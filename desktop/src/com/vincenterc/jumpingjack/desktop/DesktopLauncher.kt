cae95c1ad8858dcad02aeca182b7d5e161f1aeed
vincenterc <chung.menghsun@gmail.com>
2020-09-10 20:19:04 +0800

    Init

---
+++ desktop/src/com/vincenterc/jumpingjack/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.vincenterc.jumpingjack.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.vincenterc.jumpingjack.JumpingJackGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(JumpingJackGame(), config)
+    }
+}
